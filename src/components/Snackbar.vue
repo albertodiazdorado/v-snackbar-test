16b6e0802cffaf3b7bcfb56c6b98f286a9f1e436
Alberto Diaz <alberto.diazdorado@tngtech.com>
2020-10-20 16:04:00 +0200

    Improve style and fix test

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -1,6 +1,14 @@
 <template>
-  <v-snackbar :value="true" :timeout="-1">
+  <v-snackbar
+    :value="snackbar"
+    :timeout="0"
+    color="red"
+    content-class="v-snack-content"
+  >
     <div>{{ text }}</div>
+    <v-icon @click="snackbar = false" class="close">
+      mdi-close
+    </v-icon>
   </v-snackbar>
 </template>
 
@@ -10,5 +18,30 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class Snackbar extends Vue {
   @Prop() private text!: string;
+  private snackbar = true;
 }
 </script>
+
+<style>
+.v-snack {
+  height: auto !important;
+  position: static !important;
+  padding: 5px;
+}
+
+.v-snack__wrapper {
+  min-width: 25em !important;
+  max-width: 25em !important;
+}
+
+.v-snack__wrapper.theme--dark {
+  background-color: red !important;
+}
+
+.v-snack-content {
+  display: flex;
+  align-items: flex-start !important;
+  padding: 8px 8px !important;
+  background-color: grey;
+}
+</style>
--- tests/unit/Snackbar.spec.ts
+++ tests/unit/Snackbar.spec.ts
@@ -1,14 +1,24 @@
-import { mount } from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils";
 import Snackbar from "@/components/Snackbar.vue";
 import Vue from "vue";
-import Vuetify from "vuetify";
+import VuetifyDefault, { Vuetify } from "vuetify";
 
-Vue.use(Vuetify);
+Vue.use(VuetifyDefault);
+
+const localVue = createLocalVue()
 
 describe("Snackbar.vue", () => {
+  let vuetify: Vuetify;
+
+  beforeEach(() => {
+    vuetify = new VuetifyDefault()
+  })
+
   it("renders", () => {
     const text = "new message";
     const wrapper = mount(Snackbar, {
+      localVue,
+      vuetify,
       propsData: { text }
     });
     expect(wrapper.text()).toMatch(text);
